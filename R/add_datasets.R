659cec7c957d0245173fe2def9b68bae88821696
Alea Wilbur-Mujtaba <awilbu2@uic.edu>
2021-07-29 14:30:50 -0500

    Tried loading package and failed. Commenting out all lines so it doesn't try to recreate recruits or boward when loading the package.

--- R/add_datasets.R
+++ R/add_datasets.R
@@ -1,15 +1,16 @@
-library(tidyverse)
-library(devtools)
-library(roxygen2)
-library(testthat)
-library(fs)
 
-recruits <- read_csv("recruits.csv")
-usethis::use_data(recruits)
+# library(tidyverse)
+# library(devtools)
+# library(roxygen2)
+# library(testthat)
+# library(fs)
+
+# recruits <- read_csv("recruits.csv")
+# usethis::use_data(recruits)
 
 
 # install package created
 #devtools::install_github("AleaWM/cuppackage")
 
-broward <- read_csv("BrowardDisasters.csv")
-usethis::use_data(broward)
+# broward <- read_csv("BrowardDisasters.csv")
+# usethis::use_data(broward)
