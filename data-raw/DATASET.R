ed67e4325ee89e3804888592714a84ace3608a66
Alea Wilbur-Mujtaba <awilbu2@uic.edu>
2021-07-30 11:30:37 -0500

    Created data-raw for creating datasets. Doesn't run file, just saves it as documentation for how I created the datasets. TODO: add cleaning code from lesson plan file.

---
+++ data-raw/DATASET.R
@@ -0,0 +1,29 @@
+## code to prepare `DATASET` dataset goes here
+# usethis::use_data(DATASET, overwrite = TRUE)
+
+
+# library(tidyverse)
+# library(devtools)
+# library(roxygen2)
+# library(testthat)
+# library(fs)
+
+# recruits <- read_csv("recruits.csv")
+# usethis::use_data(recruits)
+
+
+# recruits <- read_csv("recruits.csv")
+# usethis::use_data(recruits)
+
+# install package created
+# devtools::install_github("AleaWM/cuppackage")
+
+# broward <- read_csv("BrowardDisasters.csv")
+# usethis::use_data(broward)
+
+# texasenergy <- read.table(file = "clipboard", sep = "\t", header = TRUE)
+# texasenergy
+# usethis::use_data(texasenergy)
+#
+# texastemperature <- read.table(file = "clipboard", sep = "\t", header = TRUE)
+# usethis::use_data(texastemperature)
