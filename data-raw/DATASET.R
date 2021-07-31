a6b05b59793635753d3825eea9fed44f1e53ce33
Alea Wilbur-Mujtaba <awilbu2@uic.edu>
2021-07-30 21:23:21 -0500

    drafted other datasets to be uploaded. TODO: Finish taking code from other files.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,18 +8,39 @@
 # library(testthat)
 # library(fs)
 
-# recruits <- read_csv("recruits.csv")
-# usethis::use_data(recruits)
-
-
-# recruits <- read_csv("recruits.csv")
-# usethis::use_data(recruits)
-
 # install package created
 # devtools::install_github("AleaWM/cuppackage")
 
+# recruits <- read_csv("recruits.csv")
+# usethis::use_data(recruits)
+
+
+DisasterDeclarations <- read_xlsx("C:/Users/aleaw/OneDrive/Desktop/PhD Spring 2021/Project Data/DisasterDeclarationsSummaries.xlsx")
+
+
+DisasterSummary<- DisasterDeclarations %>%
+  select(femaDeclarationString:declarationTitle, fipsStateCode:declarationRequestNumber) %>%
+  filter(designatedArea == c("Broward (County)", "Miami-Dade (County)"),
+         fipsCountyCode > 0) %>%
+  group_by(fipsStateCode, fipsCountyCode, designatedArea, fyDeclared, incidentType) %>%
+  summarize(declarations = n())
 # broward <- read_csv("BrowardDisasters.csv")
 # usethis::use_data(broward)
+
+disasterswide <-pivot_wider(DisasterSummary,
+                            names_from = c(fyDeclared),
+                            names_sep = "_",
+                            values_from = c(declarations))
+
+Claims <- read_csv("C:/Users/aleaw/OneDrive/Desktop/PhD Spring 2021/Project Data/SampleFiles/FEMAClaims.csv")
+
+claimssummary <- Claims %>%
+  select(countyCode, state, yearOfLoss)%>%
+  filter(countyCode == c("12011" , "12086"),
+         yearOfLoss > 1999) %>%
+  group_by(countyCode, state, yearOfLoss) %>%
+  summarize(claims = n())
+usethis::use_data(claimssummary)
 
 # texasenergy <- read.table(file = "clipboard", sep = "\t", header = TRUE)
 # texasenergy
